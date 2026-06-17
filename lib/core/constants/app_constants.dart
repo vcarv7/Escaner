760549a67eb8eadbe2b1089f114aed59bfa71d5c
vcarv7 <victorcv326@gmail.com>
2026-06-17 12:43:05 -0400

    fix: actualizar la URL de descarga del CSV en AppConstants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,5 +4,5 @@ class AppConstants {
   static const int maxCodeLength = 15;
   static const int pageSize = 50;
   static const Duration scanCooldown = Duration(milliseconds: 2800);
-  static const String csvUrl = ''; // PONER URL DESCARGA AQUI
+  static const String csvUrl = 'https://raw.githubusercontent.com/vcarv7/Escaner/main/tb_dpersona_activos.csv';
 }
